728e1b2abe6956baa82785cebc0577c707d0a433
CoreForce2000 <46403786+CoreForce2000@users.noreply.github.com>
2023-10-24 22:23:10 +0200

    use bodyParser

--- server/index.mjs
+++ server/index.mjs
@@ -3,12 +3,13 @@ import cors from "cors";
 import "./loadEnvironment.mjs";
 import "express-async-errors";
 import data from "./routes/data.mjs";
+import bodyParser from "body-parser";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json())
 
 // Load the /posts routes
 app.use("/data", data);
